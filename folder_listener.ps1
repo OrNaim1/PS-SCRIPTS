0c20c88ceea49c095c0d7475734a69e5dea9306b
OrNaim1 <or.naim70@gmail.com>
2024-09-05 13:12:17 +0300

    Add files via upload

---
+++ folder_listener.ps1
@@ -0,0 +1,19 @@
+﻿$sourceFolder = "" #Path of the source folder
+$destinationFolder = "" #Path of the destination folder
+$files = Get-ChildItem -Path $sourceFolder
+foreach ($file in $files) {
+    $destinationPath = Join-Path -Path $destinationFolder -ChildPath $file.Name
+    Move-Item -Path $file.FullName -Destination $destinationPath -Force
+    $smtpServer = "" #smtp server address
+    $emailFrom = "" #email sender address
+    $emailTo = "orn@hertz.co.il"
+    $subject = ($file.BaseName -replace '_', ':')
+    $body = "A new file has been added to the folder: " + $destinationPath
+    $smtp = New-Object Net.Mail.SmtpClient($smtpServer)
+    $msg = New-Object Net.Mail.MailMessage($emailFrom, $emailTo, $subject, $body)
+    $attachment = New-Object Net.Mail.Attachment($destinationPath)
+    $msg.Attachments.Add($attachment)
+    $smtp.Send($msg)
+    $attachment.Dispose()
+    $msg.Dispose()
+}
